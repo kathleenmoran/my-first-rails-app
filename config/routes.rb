b746fb5896a3a6bafe34d83149b425188280c2b5
Kathleen Moran <kathleenmoran10@gmail.com>
2022-07-17 13:54:37 -0400

    Updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    root 'cars#index'
+    resources :cars
+  end
 end
